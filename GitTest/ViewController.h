f3d911c6e3e7d4197a2b201e743cc5c70592957e
xiaoyuzi <18205626240@163.com>
2018-01-17 12:29:12 +0800

    Initial Commit

---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by weiyun on 2018/1/17.
+//  Copyright © 2018年 wy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by weiyun on 2018/1/17.
+//  Copyright © 2018年 wy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
